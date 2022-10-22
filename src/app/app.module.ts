a137559e89132475a4931d2d7203e58b4f88f837
nadiakramer <nadia.kramer@youengineering.ch>
2022-10-22 15:56:16 +0200

    dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './components/pages/dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
