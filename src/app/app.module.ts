382edaea92e01bb678c9e38034fb9cd86377a158
FXFrosties <Jolasmirein5498>
2022-10-22 15:46:23 +0200

    fixed directory issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryOverviewComponent } from './components/category-overview/category-overview.component';
-import { CategoryCardComponent } from './category-card/category-card.component';
+import { CategoryCardComponent } from './components/category-card/category-card.component';
 
 @NgModule({
   declarations: [
