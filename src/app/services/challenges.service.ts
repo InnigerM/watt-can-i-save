788698029010d926f2cc74bd2b5c699364ed5b3d
yveswehrli <yves.wehrli@bluewin.ch>
2022-10-23 09:23:37 +0200

    Calculate statistics

--- src/app/services/challenges.service.ts
+++ src/app/services/challenges.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Challenges } from '../models/challenges.model';
+import { Statistics } from '../models/statistics';
 import { CHALLENGES_KEY } from '../utils';
+import { ElectricityPriceService } from './electricity-price.service';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +15,7 @@ export class ChallengesService {
     Challenges[]
   >([]);
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private electricityPriceService: ElectricityPriceService) {
     // Load empty challenges from assets if localstorage is empty
     if (localStorage.getItem(CHALLENGES_KEY) === null) {
       this.httpClient
@@ -49,6 +51,32 @@ export class ChallengesService {
       .map((challenge) => (challenge.solved = status));
     this.saveToLocalStorage();
   }
+
+  getStatistics(category: string = null): Statistics {
+    const price = this.electricityPriceService.getPrice().value;
+
+    const statistics: Statistics = {
+      challengesSolved: 0,
+      potentialSavings: 0,
+      savings: 0,
+      totalAmount: 0
+    }
+
+    const challenges = category !== null ? this.challenges.value.filter(challenge => challenge.category === category) : this.challenges.value
+
+   challenges.forEach(challenge => {
+      if (challenge.solved) {
+        statistics.challengesSolved += 1;
+        statistics.savings += (challenge.potentialKwhSaved * price) / 100
+      } else {
+        statistics.potentialSavings += (challenge.potentialKwhSaved * price) / 100;
+      }
+    });
+
+    statistics.totalAmount = statistics.potentialSavings + statistics.savings;
+    return statistics;
+  }
+
 
   private saveToLocalStorage() {
     localStorage.setItem(CHALLENGES_KEY, JSON.stringify(this.challenges.value));
