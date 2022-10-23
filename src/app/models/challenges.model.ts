84333c5bed9d7efdfa0ef2700f5bc612449d1745
Marco Inniger <marco.inniger@gmail.com>
2022-10-23 09:16:09 +0200

    feat: add solving, filtering challenges

--- src/app/models/challenges.model.ts
+++ src/app/models/challenges.model.ts
@@ -1,4 +1,5 @@
 export interface Challenges {
+  id: number;
   title: string;
   description: string;
   longText: string;
--- src/app/services/challenges.service.ts
+++ src/app/services/challenges.service.ts
@@ -1,28 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Challenges } from '../models/challenges.model';
 import { CHALLENGES_KEY } from '../utils';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChallengesService {
-
   private FILE_PATH: string = '/assets/challenges.json';
-  public challenges: BehaviorSubject<Challenges[]>;
+  public challenges: BehaviorSubject<Challenges[]> = new BehaviorSubject<
+    Challenges[]
+  >([]);
 
   constructor(private httpClient: HttpClient) {
     // Load empty challenges from assets if localstorage is empty
-    if(localStorage.getItem(CHALLENGES_KEY) === null) {
-      this.httpClient.get<Challenges[]>(this.FILE_PATH)
-        .subscribe(challenges => {
-          this.challenges.next(challenges) ;
+    if (localStorage.getItem(CHALLENGES_KEY) === null) {
+      this.httpClient
+        .get<Challenges[]>(this.FILE_PATH)
+        .subscribe((challenges) => {
+          this.challenges.next(challenges);
           this.saveToLocalStorage();
         });
     } else {
-      this.challenges.next(JSON.parse(localStorage.getItem(CHALLENGES_KEY)));
+      this.challenges.next(JSON.parse(localStorage.getItem(CHALLENGES_KEY)!));
     }
+  }
+
+  getChallengesByCategory(category: string): Observable<Challenges[]> {
+    return this.challenges.pipe(
+      map((challenges) =>
+        challenges.filter((challenge) => challenge.category === category)
+      )
+    );
+  }
+
+  setChallengeSolved(id: number, status: boolean): void {
+    this.challenges.value
+      .filter((challenge) => challenge.id === id)
+      .map((challenge) => (challenge.solved = status));
+    this.saveToLocalStorage();
   }
 
   private saveToLocalStorage() {
