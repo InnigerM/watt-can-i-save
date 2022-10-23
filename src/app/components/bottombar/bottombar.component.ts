7477f079344d860f53dd065e6c357157a7b764b5
Marco Inniger <marco.inniger@gmail.com>
2022-10-23 10:14:46 +0200

    fix: bottom nav issues

--- src/app/components/bottombar/bottombar.component.ts
+++ src/app/components/bottombar/bottombar.component.ts
@@ -4,29 +4,30 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 @Component({
   selector: 'app-bottombar',
   templateUrl: './bottombar.component.html',
-  styleUrls: ['./bottombar.component.scss']
+  styleUrls: ['./bottombar.component.scss'],
 })
 export class BottombarComponent implements OnInit {
   visible = false;
 
   constructor(private router: Router) {
     router.events.subscribe((val) => {
-      if(val instanceof NavigationEnd){
-        if(val.url === '/rooms' || val.url === '/'){
+      if (val instanceof NavigationEnd) {
+        if (
+          val.url === '/rooms' ||
+          val.url === '/' ||
+          /categories\/\w{4,10}\/challenges\/\d{1,3}/.test(val.url)
+        ) {
           this.visible = false;
-        }else{
+        } else {
           this.visible = true;
         }
       }
-    }) 
-  };
-
-  ngOnInit(): void {
-
+    });
   }
 
-  submit(page: string): void{
+  ngOnInit(): void {}
+
+  submit(page: string): void {
     this.router.navigate([page]);
   }
-
 }
